c45263797d19ef2569ec38b43c6b360dfe210d17
disko998 <disko998@gmail.com>
2023-06-03 20:48:18 +0200

    add container

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,33 @@
-import React from "react";
-import "./Header.scss";
-import Logo from "../../assets/logo/logo.svg";
-import Cart from "../../assets/cart/cart.svg";
-import ProfileIcon from "../../assets/profile-icon.svg";
-import HamburgerMenu from "../../assets/hamburger-menu.svg";
+import React from 'react'
+import './Header.scss'
+import Logo from '../../assets/logo/logo.svg'
+import Cart from '../../assets/cart/cart.svg'
+import ProfileIcon from '../../assets/profile-icon.svg'
+import { ReactComponent as HamburgerMenu } from '../../assets/hamburger-menu.svg'
 
 const Header = () => {
-  return (
-    <div className="wrapper">
-      <div className="header">
-        <img src={Logo} alt="logo" width={242} height={60.91} />
-        <div className="nav-menu">
-          <ul>
-            <li>MAKING YOUR OWN</li>
-            <li>SHOP</li>
-            <li>RESOURCES</li>
-            <li>CONTACT</li>
-          </ul>
-          <button className="nav-button">MY ACCOUNT</button>
-          <img src={Cart} alt="cart" width={97} height={51} />
+    return (
+        <div className='wrapper'>
+            <div className='header'>
+                <img src={Logo} alt='logo' width={242} height={60.91} />
+                <div className='nav-menu'>
+                    <ul>
+                        <li>MAKING YOUR OWN</li>
+                        <li>SHOP</li>
+                        <li>RESOURCES</li>
+                        <li>CONTACT</li>
+                    </ul>
+                    <button className='nav-button'>MY ACCOUNT</button>
+                    <img src={Cart} alt='cart' width={97} height={51} />
+                </div>
+                <div className='responsive-header'>
+                    <img src={ProfileIcon} alt='' width={30} />
+                    <img src={Cart} alt='' width={50} />
+                    <HamburgerMenu />
+                </div>
+            </div>
         </div>
-        <div className="responsive-header">
-          <img src={ProfileIcon} alt="" width={30} />
-          <img src={Cart} alt="" width={50} />
-          <img src={HamburgerMenu} alt="" width={34} />
-        </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Header;
+export default Header
